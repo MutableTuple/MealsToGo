2b101e024f4ef5f3c699a41fecc0c6d45b33b718
MutableTuple <mutabletuple39@gmail.com>
2022-12-16 10:36:51 +0530

    updated some files

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { Platform, SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+
+// const isAndroid = Platform.OS ==='android';
+// console.log(StatusBar.currentHeight);
+// console.log(ExpoStatusBar.currentHeight);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <> are called fragments and they let us render two compoenent side by side
+    // SafeAreaView is only for IOS
+
+    <> 
+    <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+            <Searchbar
+          placeholder="Search"
+         
+        />
+        </View>
+
+        <View style={styles.list}>
+          <Text style={{color:'white'}}>List</Text>
+        </View>
+    </ SafeAreaView>
+    <ExpoStatusBar />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#0C0404",
   },
 });
